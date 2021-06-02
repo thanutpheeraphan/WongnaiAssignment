f038d7195045ec12ed741521e156e6b55e78b474
Thanut Pheeraphan <thanut@wongnai.com>
2021-06-02 17:48:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wongnaiassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wongnaiassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
